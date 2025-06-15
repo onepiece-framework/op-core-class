24f6fed517493880872087c716823f3caa46c4c2
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-15 22:15:52 +0900

    New: Notice.class.php

---
+++ Notice.class.php
@@ -0,0 +1,32 @@
+<?php
+/**	op-core-class:/Notice.class.php
+ *
+ * @genesis    ????-??-??  op-core-4:/OnePiece.class.php
+ * @created    2016-11-17  op-core-7:/Notice.class.php
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/**	Notice
+ *
+ * @deprecated 2025-06-15
+ * @created    2016-11-17  op-core-7:/Notice
+ * @version    2.0
+ * @package    op-core
+ * @subpackage class
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+class Notice
+{
+	/**	use
+	 *
+	 */
+	use OP_CORE, OP_CI;
+	use OP_ERROR;
+}
