4fe106fa85b68bbc57d432ebce02b13bd1429c98
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-15 22:25:06 +0900

    Add: Error.class.php | class > Error

--- Error.class.php
+++ Error.class.php
@@ -11,3 +11,19 @@
  *
  */
 namespace OP;
+
+/**	Error
+ *
+ * @created    2014-02-18  by op-core-5:/Error
+ * @updated    2016-01-16  at op-core-5:/OP\Error
+ * @renamed    2016-11-17  to op-core-7:/OP\Notice
+ * @rebirth    2025-06-11  op-core-class:/OP\Error
+ * @version    1.0
+ * @package    op-core
+ * @subpackage class
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+class Error
+{
+}
