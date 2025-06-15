b8ceee43e4f222a0f85e29eae198cf44286e4927
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-15 22:02:08 +0900

    Add: Unit.class.php | PHPDoc

---
+++ Unit.class.php
@@ -0,0 +1,11 @@
+<?php
+/**	op-core-class:/Unit.class.php
+ *
+ * @genesis    ????-??-??  at op-core-5:/$this->Unit()
+ * @created    2016-11-28  by op-core-7:/Unit.class.php
+ * @porting    2025-06-15  to op-core-class:/Unit.class.php
+ * @version    1.0
+ * @package    op-core-class
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
