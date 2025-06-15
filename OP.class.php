7763c5ab3bb6e11f089edf619ad3cda2c6aac611
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-15 21:03:15 +0900

    Add: OP.class.php | PHPDoc

---
+++ OP.class.php
@@ -0,0 +1,12 @@
+<?php
+/**	op-core-class:/OP.class.php
+ *
+ * The "OP" is the Great Treasure that Connects All!!
+ *
+ * @created    2022-09-30
+ * @rebirth    2025-06-11
+ * @version    1.0
+ * @package    op-core-class
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
