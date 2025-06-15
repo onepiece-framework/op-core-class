37f5c8caaf3fcfdfae021114a5e12cb79e400669
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-15 22:24:18 +0900

    Add: Error.class.php | PHPDoc

---
+++ Error.class.php
@@ -0,0 +1,8 @@
+<?php
+/**	op-core-class:/Error.class.php
+ *
+ * @created    2014-11-29  at op-core-5:/Error.class.php
+ * @rebirth    2025-06-11  to op-core-class:/Error.class.php
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
